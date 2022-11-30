b11995735b054cbcf6638e4ff035078db0a3fc81
akash-patel-0906 <76273320+akash-patel-0906@users.noreply.github.com>
2022-11-30 11:59:38 -0600

    Add files via upload

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -1,21 +1,34 @@
 from flask import Flask, render_template, request, flash
-from textblob import TextBlob
-import urllib.request
-import re
-from youtube_transcript_api import YouTubeTranscriptApi
-import webbrowser
 from wordcloud import WordCloud,  ImageColorGenerator
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from PIL import Image
 from youtube_search import YoutubeSearch
 from pytube import YouTube
+import pandas as pd
+from LeXmo import LeXmo
+import plotly.express as px
+import plotly.graph_objects as go
+
 
 app = Flask(__name__)
-app.secret_key = "asdasdjkjk1kj2kljkjk"
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/static/images/<cropzonekey>')
-def images(cropzonekey):
-    return render_template("images.html", title=cropzonekey)
+app.secret_key = "asdasdjkjk1kj2kljkjk"
+app.config["CACHE_TYPE"] = "null"
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 #export FLASK_ENV=development
 #index file:
@@ -27,47 +40,92 @@ def index():
 @app.route("/analysis", methods=['POST', 'GET'])
 #conduct sentiment analysis
 def analysis():
+   
+   #contains dictionary of emotions
+    other_emotions_dictionary={
+        'positive':0,
+        'negative':0,
+        'anger':0,
+        'anticipation':0, 
+        'disgust':0,
+        'fear':0,
+        'joy':0,
+        'sadness':0,
+        'surprise':0,
+        'trust':0
+    }
 
-    #generates urls
+    #generates urls from url_request
     input = str(request.form['topic_input'])
     ids=[]
-    results = YoutubeSearch(input, max_results=10).to_dict()
+    results = YoutubeSearch(input, max_results=30).to_dict()
     for v in results:
         ids.append('https://www.youtube.com' + v['url_suffix'])
 
 
-    #string containing titles
+    #creates string using list of titles
     list_of_info=""
-    for id in range (0,min(10,len(ids))):
+    for id in range (0,min(30,len(ids))):
         title = ids[id]
         video = YouTube(title)
-        print(video.title)
         list_of_info+=video.title + "\n"
 
-    #sentiment calculation
-    sentiment = TextBlob(list_of_info).sentiment.polarity
+ 
 
-    #add negative boundary here:
-    #add positive bounday here:
-
-
-    sentiment_empirical =""
-    if sentiment<0:
-        sentiment_empirical="negative"
-    elif sentiment>0:
-        sentiment_empirical="positive"
-    else:
-        sentiment_empirical="neutral"
+    #calculate emotions utizling Lexmo (unigram-sentiment analyzer)
+    emotions=LeXmo.LeXmo(list_of_info)
+    
+    #adds emotions to dicionary
+    for key in other_emotions_dictionary:
+        other_emotions_dictionary[key] = emotions[key]
 
 
-    overall = "The overall sentiment is: " + str(sentiment) + " which is " + sentiment_empirical
+    #generates sentiment values
+    sentiment = max(other_emotions_dictionary, key=other_emotions_dictionary.get)
+    sentiment_empirical = other_emotions_dictionary.get(sentiment)
+    
+    #final sentiment
+    overall = "The overall sentiment of " +str(input) + " is " + str(sentiment) + " with a value of " + str(sentiment_empirical)
+   
+    #generate graph
+    names = list(other_emotions_dictionary.keys())
+    values = list(other_emotions_dictionary.values())
 
-    #generation of WordCloud
-    wordcloud = WordCloud(background_color="white",max_words=200, contour_width=3, contour_color='firebrick', collocations=False).generate(list_of_info)
-    plt.figure(figsize=[20,10])
+    plt.bar(range(len(other_emotions_dictionary)), values, tick_label=names)
+    plt.xticks(range(10)) # add loads of ticks
+    plt.grid()
+
+    plt.gca().margins(x=0)
+    plt.gcf().canvas.draw()
+    
+    tl = plt.gca().get_xticklabels()
+    maxsize = max([t.get_window_extent().width for t in tl])
+    m = 0.2 # inch margin
+    s = maxsize/plt.gcf().dpi*10+2*m
+    margin = m/plt.gcf().get_size_inches()[0]
+
+    plt.gcf().subplots_adjust(left=margin, right=1.-margin)
+    plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
+
+    plt.savefig('static/other_emotions.png',dpi=100)
+
+    plt.clf()
+    plt.cla()
+    plt.close()
+
+ 
+
+    #generation of WordCloud from words
+    wordcloud = WordCloud(background_color="white",max_words=200, contour_width=1, contour_color='firebrick', collocations=False).generate(list_of_info)
+    plt.figure(figsize=[80,40])
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
-    plt.savefig('static/test.png')
+    plt.savefig('static/word_cloud.png',dpi=100)
+
+    plt.clf()
+    plt.cla()
+    plt.close()
+
 
     flash(overall)
     return render_template("analysis.html")
@@ -86,3 +144,4 @@ if __name__ == "__main__":
             return response
     app.run(host="127.0.0.1", port=5001)
     
+#export FLASK_DEBUG=1
